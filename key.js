ecb9c394509f0a2adf86b28df88cd8f170add452
Desiree Tien <desireetien@Aphesis.local>
2017-12-06 16:06:46 -0600

    setting up assignment

---
+++ key.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'jTyOdspFQqyl7sjwuQoz2XHir',
+  consumer_secret: 'ERq73aTybN1P9OLszfBUkhvZjahYWm4aqNo0l6ByKvXbkHYZlf',
+  access_token_key: '938521755160899584-0PmMxACUm96KzFd9mjaNKJzbZeWfXZ8',
+  access_token_secret: 'Vw5wXa7oQ49PYqxYxPqbEg1ij9dWRjD8mfHasUFu4s7Lb',
+}
+
+module.exports = twitterKeys;
