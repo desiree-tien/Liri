f91548551c94f4a88037aa93c1da51d4859beb6e
Desiree <32805467+desiree-tien@users.noreply.github.com>
2017-12-08 09:52:50 -0600

    Update keys2.js

--- keys2.js
+++ keys2.js
@@ -1,6 +1,7 @@
 var spotify = new Spotify({
-  id:"xxxxxxxxxxxxxxxxx",
-  secret: "xxxxxxxxxxxxxxxxxxxx"
+  id:"3269877592fa4ac78df1581de91e4d11
+",
+  secret: "efac7b8a1934420fbf67380f1d5a34d7"
 });
 
 
